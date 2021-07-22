c2be3b5481d57801a2702d079e8bfcc199c43448
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-07-22 16:32:03 +0200

    Support skipping the preconditioner in elliptic solves

--- tests/Unit/ParallelAlgorithms/LinearSolver/Actions/Test_MakeIdentityIfSkipped.cpp
+++ tests/Unit/ParallelAlgorithms/LinearSolver/Actions/Test_MakeIdentityIfSkipped.cpp
@@ -3,6 +3,11 @@
 
 #include "Framework/TestingFramework.hpp"
 
+#include <cstddef>
+#include <tuple>
+#include <utility>
+
+#include "DataStructures/DataBox/DataBox.hpp"
 #include "DataStructures/DataBox/Tag.hpp"
 #include "DataStructures/DenseVector.hpp"
 #include "Framework/ActionTesting.hpp"
@@ -10,6 +15,7 @@
 #include "Parallel/Actions/TerminatePhase.hpp"
 #include "Parallel/PhaseDependentActionList.hpp"
 #include "ParallelAlgorithms/LinearSolver/Actions/MakeIdentityIfSkipped.hpp"
+#include "Utilities/Gsl.hpp"
 #include "Utilities/Literals.hpp"
 #include "Utilities/TMPL.hpp"
 
@@ -25,10 +31,31 @@ struct SourceTag : db::SimpleTag {
   using type = DenseVector<double>;
 };
 
+struct CheckRunIfSkippedTag : db::SimpleTag {
+  using type = bool;
+};
+
 struct TestLinearSolver {
   using options_group = TestOptionsGroup;
   using fields_tag = FieldsTag;
   using source_tag = SourceTag;
+};
+
+struct RunIfSkipped {
+  template <typename DbTagsList, typename... InboxTags, typename Metavariables,
+            typename ArrayIndex, typename ActionList,
+            typename ParallelComponent>
+  static std::tuple<db::DataBox<DbTagsList>&&> apply(
+      db::DataBox<DbTagsList>& box,
+      const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+      const Parallel::GlobalCache<Metavariables>& /*cache*/,
+      const ArrayIndex& /*array_index*/, const ActionList /*meta*/,
+      const ParallelComponent* const /*meta*/) noexcept {
+    db::mutate<CheckRunIfSkippedTag>(
+        make_not_null(&box),
+        [](const gsl::not_null<bool*> flag) { *flag = true; });
+    return {std::move(box)};
+  }
 };
 
 template <typename Metavariables>
@@ -41,12 +68,12 @@ struct ElementArray {
           typename Metavariables::Phase, Metavariables::Phase::Initialization,
           tmpl::list<ActionTesting::InitializeDataBox<
               tmpl::list<Convergence::Tags::HasConverged<TestOptionsGroup>,
-                         FieldsTag, SourceTag>>>>,
+                         FieldsTag, SourceTag, CheckRunIfSkippedTag>>>>,
       Parallel::PhaseActions<
           typename Metavariables::Phase, Metavariables::Phase::Testing,
-          tmpl::list<
-              LinearSolver::Actions::MakeIdentityIfSkipped<TestLinearSolver>,
-              Parallel::Actions::TerminatePhase>>>;
+          tmpl::list<LinearSolver::Actions::make_identity_if_skipped<
+                         TestLinearSolver, RunIfSkipped>,
+                     Parallel::Actions::TerminatePhase>>>;
 };
 
 struct Metavariables {
@@ -62,10 +89,13 @@ void test_make_identity_if_skipped(const bool skipped) {
   ActionTesting::MockRuntimeSystem<Metavariables> runner{{}};
   ActionTesting::emplace_component_and_initialize<element_array>(
       make_not_null(&runner), 0,
-      {has_converged, DenseVector<double>{3, 1.}, DenseVector<double>{3, 2.}});
+      {has_converged, DenseVector<double>{3, 1.}, DenseVector<double>{3, 2.},
+       false});
   ActionTesting::set_phase(make_not_null(&runner),
                            Metavariables::Phase::Testing);
-  ActionTesting::next_action<element_array>(make_not_null(&runner), 0);
+  while (not ActionTesting::get_terminate<element_array>(runner, 0)) {
+    ActionTesting::next_action<element_array>(make_not_null(&runner), 0);
+  }
   const auto get_tag = [&runner](auto tag_v) -> decltype(auto) {
     using tag = std::decay_t<decltype(tag_v)>;
     return ActionTesting::get_databox_tag<element_array, tag>(runner, 0);
@@ -73,6 +103,7 @@ void test_make_identity_if_skipped(const bool skipped) {
   CHECK(get_tag(SourceTag{}) == DenseVector<double>{3, 2.});
   CHECK(get_tag(FieldsTag{}) ==
         (skipped ? DenseVector<double>{3, 2.} : DenseVector<double>{3, 1.}));
+  CHECK(get_tag(CheckRunIfSkippedTag{}) == skipped);
 }
 
 }  // namespace
